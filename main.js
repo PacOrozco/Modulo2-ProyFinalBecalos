d0238f5b595ac530caf8c251a8201629bf1e9716
Francisco Orozco <francoroz99@gmail.com>
2024-12-05 20:37:20 -0600

    agregar mas archivos html, diseño de footer

---
+++ main.js
@@ -0,0 +1,10 @@
+// funcion = pedacito de codigo reutilizable, comentarios solo en una linea
+
+/*para hacer comentarios
+multilinea 
+
+function "accion a realizar" (anotacion) {}*/
+
+function redirigir(url) {
+    window.location.href = url;   
+}
